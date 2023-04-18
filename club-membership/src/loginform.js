5f696c8506175028e664e470480f9a6b4367a14f
melvinjames <melvin.gacheru@student.morigaschool.com>
2023-04-18 14:36:37 +0300

    completed assignment

---
+++ club-membership/src/loginform.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+const LoginForm = () =>{
+
+  return    <div classname = "cover">
+  
+<form>
+<h1>LOGIN</h1>
+<input type = "text"className="log" placeholder ="username"/>
+
+
+<input type = "password"className="log" placeholder = "password"/>
+
+
+<input type = "text" className=" log"placeholder ="member number"/>
+
+
+<button>submitt</button>
+
+</form>
+    </div>
+}
+export default LoginForm
